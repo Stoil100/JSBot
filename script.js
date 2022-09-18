665dfd79a43efe316d39cb871670c74de65603ed
User <toli100ilov@gmail.com>
2022-09-18 22:37:41 +0300

    Finnished the responding function

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-let lastRandomNumber;
+let randomNumber;
+let input1 = document.getElementById('input1');
+const response = document.getElementById('responseArea');
+const button = document.querySelector('.highlight');
+const setText = ["Hello", "Хеллоу", "Hi", "драсте"];
+const randomText = ["Hey, I'm bot", "Здрастииии", "Добро утроо", "Heya!", "Здравей!"];
+const helloText=randomText.concat("from the other side","toaster");
 
-let input1=document.getElementById('input1');
-const response=document.getElementById('responseArea');
-const button=document.querySelector('.highlight');
-const randomText=["Hey, I'm bot" , "Здрастииии" , "Добро утроо" , "Heya!" , "Здравей!"];
-
-function greetingsF(){
-let randomNumber=Math.round((Math.random())*4);
-
-if(lastRandomNumber===randomNumber)randomNumber=Math.round((Math.random())*4);
-lastRandomNumber=randomNumber;
-return (input1.value==='Hello'||input1.value==='Хеллоу'||input1.value==='Hi'||input1.value==='драсте'
- ?response.textContent=`${randomText[randomNumber]}`:response.textContent='');
+function greetingsF() {
+    setText.includes(input1.value)&&input1.value!=="Hello" ? 
+    response.textContent=`${randomText[randomNumber = Math.round((Math.random()) * 4)]}`:input1.value==="Hello"?
+    response.textContent=`${helloText[randomNumber = Math.round((Math.random()) * 6)]}`:response.textContent='';
 };
 
-button.addEventListener('click',greetingsF);
+button.addEventListener('click', greetingsF);

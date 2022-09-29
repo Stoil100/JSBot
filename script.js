45774aa917f0251ff83b279dc2f57ffe5ce550c4
User <toli100ilov@gmail.com>
2022-09-29 21:34:10 +0300

    Finnished time function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let randomNumber;
+let lastNumber;
 let input = document.getElementById('input1');
 const response = document.getElementById('responseArea');
 const button = document.querySelector('.highlight');
@@ -6,7 +7,7 @@ const button = document.querySelector('.highlight');
 const setText = {
     setHello:["Хеллоу","Hi","драсте"],
     setLike:["I like","аз обичам","аз харесвам"],
-    setTime:["Колко е часа?","Час","Време"],
+    setTime:["Колко е часа?","Час","Време","hour"],
     setWeather:["Weather","прогноза","вали","градус","ракия","сняг","дъжд"]
 };
 //responding text
@@ -14,7 +15,7 @@ const randomText = {
     greeting:["Hey, I'm bot", "Здрастииии","Добро утроо","Heya!"],
     hello:["Hey, I'm bot", "Здрастииии","Добро утроо","Heya!","from the other side","is it me you are looking for?"],
     like:["Кондьо", "Джена" ,"Джорджано", "Гери и Никол"],
-   /* time:[`абе май е ${time}`,`Шес бес десет, няма бе, ${actualTime} е..`,`Ако умножиш ${X} по ${Y} ще получиш unix timestamp, който лесно можеш сам да си пресметнеш и да получиш текущата дата`],
+    /*time:[`абе май е ${setInterval(trolTimeFunction,1000)}`,`Шес бес десет, няма бе, ${setInterval(actualTimeFunction,1000)} е..`],/*,`Ако умножиш ${X} по ${Y} ще получиш unix timestamp, който лесно можеш сам да си пресметнеш и да получиш текущата дата`],
     weather:[ 
         `В момента е ${weatherDescription}`,
         `За тези, които ги мързи да погледнат през прозореца - вън е ${weatherCondition}`,
@@ -23,6 +24,51 @@ const randomText = {
         `${temp} градуса, как живеете вие в тая жега, добре че съм бот!`]*/
 };
 
+function timeFunction(){
+    let currentTime=new Date();
+    let timestamp=currentTime.getTime();
+    let X;
+    let Y;
+    randomNumber=Math.round(Math.random()*2);
+    while(randomNumber===lastNumber)randomNumber=Math.round(Math.random()*2);
+    console.log(timestamp);
+    let hour=currentTime.getHours();
+    let minutes=currentTime.getMinutes();
+    let trolHour=Math.round(Math.random()*currentTime.getHours());
+    let trolMinutes=Math.round(Math.random()*currentTime.getMinutes());
+
+    if(randomNumber===2){
+    for(i=2;i<=timestamp;i++){
+        if(timestamp%i==0){
+            X=i;
+            Y=timestamp/X;
+            break;
+        }
+        else if(i===1_000_000){
+            X=timestamp;
+            Y=timestamp/X;
+            break;
+        }
+    }
+       
+    }
+    
+
+    if(hour<10)hour=`0${hour}`;
+    if(minutes<10)minutes=`0${minutes}`;
+    
+    if(trolHour<10)trolHour=`0${trolHour}`;
+    if(trolMinutes<10)trolMinutes=`0${trolMinutes}`;
+    
+    let actualTime=`${hour}:${minutes}`;
+    let trolTime=`${trolHour}:${trolMinutes}`;
+    
+    const time=[`Шес бес десет, няма бе, ${actualTime} е..`,`абе май е ${trolTime}`,
+    `Ако умножиш ${X} по ${Y} ще получиш unix timestamp, който лесно можеш сам да си пресметнеш и да получиш текущата дата`];
+
+    response.textContent=time[randomNumber];
+    lastNumber=randomNumber;   
+}
 
 function functionTree() {
     if(setText.setHello.includes(input.value)){
@@ -43,16 +89,15 @@ function functionTree() {
 
             response.textContent=randomText.like[randomNumber];
     }
-   /* else if(setText.setTime.includes(input.value)){
-        timeFunction();
+    else if(setText.setTime.includes(input.value)){
+    timeFunction();
     }
-    else if(setText.setWeather.includes(input.value)){
+    /*else if(setText.setWeather.includes(input.value)){
         weatherFunction();
     }*/
     else{
         return;
     }
 }
-
 
 button.addEventListener('click', functionTree);
